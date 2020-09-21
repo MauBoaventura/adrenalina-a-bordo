5fb2f7a9805539b59aac942d5e30e7d4af10f6d2
Mauricio <mba097@gmail.com>
2020-09-21 00:03:24 -0300

    Finish Database

--- src/database/migrations/20200920220854_create-trips.js
+++ src/database/migrations/20200920220854_create-trips.js
@@ -2,11 +2,11 @@ exports.up = function (knex, Promise) {
     return knex.schema.createTable('trips', function (table) {
         table.increments();
         
-        table.integer('vihecleId').notNullable().unsigned();
-        table.foreign('vihecleId').references('id').inTable('vihecles')
+        table.integer('vehicleId').notNullable().unsigned();
+        table.foreign('vehicleId').references('id').inTable('vehicles')
 
         table.integer('schedulingId').notNullable().unsigned();
-        table.foreign('schedulingsId').references('id').inTable('schedulings')
+        table.foreign('schedulingId').references('id').inTable('schedulings')
 
         table.timestamp('createdAt').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP'));
         // table.timestamp('updatedAt').defaultTo(knex.raw('NULL ON UPDATE CURRENT_TIMESTAMP'))
